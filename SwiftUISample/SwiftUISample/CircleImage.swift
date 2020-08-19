875ac9e61c0270fd3780c801c8e9a97f1a34333d
eugene-pc <48508998+eugene-pc@users.noreply.github.com>
2020-08-19 08:26:07 +0300

    Add files via upload
    Added source

---
+++ SwiftUISample/SwiftUISample/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  SwiftUISample
+//
+//  Created by Eugene on 8/3/20.
+//  Copyright © 2020 Jevate. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.gray, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
